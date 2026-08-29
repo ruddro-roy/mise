875cb9f15d34245db5cfafb40a4aa8a588f39cd8
Ruddro Roy <roy@ruddro.com>
2026-08-29 09:06:49 -0700

    Mount save-error toasts

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/sonner";
 import { StudioShell } from "@/components/studio/studio-shell";
 
 export function App() {
   return (
     <TooltipProvider>
       <StudioShell />
+      <Toaster />
     </TooltipProvider>
   );
 }
